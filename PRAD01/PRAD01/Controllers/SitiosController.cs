524a3ab10e32eb93e1ba062f2a34b1b09bc27b92
Greydi Martinez <greydimart@outlook.com>
2022-04-02 09:04:13 -0600

    Agregar archivos de proyecto.

---
+++ PRAD01/PRAD01/Controllers/SitiosController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using PRAD01.Models;
+using PRAD01.Controllers;
+using System.Threading.Tasks;
+
+namespace PRAD01.Controllers
+{
+    public static class SitiosController
+    {
+        // Procedimientos
+
+        // Retorna todas las filas de la tabla personas
+        public static Task<List<Sitios>> ObtenerListaSitios()
+        {
+            return DB.dbconexion.Table<Sitios>().ToListAsync();
+        }
+
+        // OPERACIONES CRUD - CREATE , READ , UPDATE , DELETE 
+        // INSERT, SELECT, UPDATE, DELETE 
+
+        // CREATE - UPDATE
+        public static Task<int> AddSitios(Sitios sitio)
+        {
+            if (sitio.ID != 0)
+            {
+                // Ejecutamos el procedimiento de Actualizacion UPDATE
+                return DB.dbconexion.UpdateAsync(sitio);
+            }
+            else
+            {
+                // Ejectuamos el procedimiento de INSERCCION de UNA PERSONA
+                return DB.dbconexion.InsertAsync(sitio);
+            }
+        }
+
+        // Obtenemos por el ID un registro de una persona
+        public static Task<Sitios> ObtenerSitio(int pid)
+        {
+            return DB.dbconexion.Table<Sitios>()
+                .Where(i => i.ID == pid)
+                .FirstOrDefaultAsync();
+        }
+
+        // Eliminamos el registro de una persona
+        public static Task<int> DelPersona(Sitios sitio)
+        {
+            return DB.dbconexion.DeleteAsync(sitio);
+        }
+    }
+}
---
+++ PRAD01/PRAD01/Views/PageSitios.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PRAD01.Models;
+using PRAD01.Controllers;
+using Plugin.Media;
+using System.IO;
+
+namespace PRAD01.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageSitios : ContentPage
+    {
+        Plugin.Media.Abstractions.MediaFile photo = null;
+
+        public PageSitios()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            CancellationTokenSource cts;
+
+            try
+            {
+                var location = await Geolocation.GetLocationAsync();
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    latitud.Text = Convert.ToString(location.Latitude);
+                    longitud.Text = Convert.ToString(location.Longitude);
+                }
+                else
+                {
+                    var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                    cts = new CancellationTokenSource();
+                    location = await Geolocation.GetLocationAsync(request, cts.Token);
+
+                    if (location != null)
+                    {
+                        latitud.Text = Convert.ToString(location.Latitude);
+                        longitud.Text = Convert.ToString(location.Longitude);
+                    }
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                fnsEx.ToString();
+                // Handle not supported on device exception
+            }
+        }
+
+
+        private Byte[] traeImagenByteArray()
+        {
+            if (photo != null)
+            {
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    Stream stream = photo.GetStream();
+                    stream.CopyTo(memory);
+                    return memory.ToArray();
+                }
+            }
+            return null;
+        }
+
+        private async void btnagregar_Clicked(object sender, EventArgs e)
+        {
+            if (photo == null)
+                return;
+
+            var site = new Sitios()
+            {
+                descripcion = descripcion.Text,
+                longitud = Convert.ToDouble(longitud.Text),
+                latitud = Convert.ToDouble(latitud.Text),
+                foto = traeImagenByteArray(),
+                fecha = fecha.Date
+            };
+
+            if (await SitiosController.AddSitios(site) > 0)
+                await DisplayAlert("Aviso", "Registro Adicionado", "OK");
+            else
+                await DisplayAlert("Aviso", "ha ocurrido un error", "OK");
+        }
+
+        private async void btnfoto_Clicked(object sender, EventArgs e)
+        {
+            photo = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "FotosApp",
+                Name = "test.jpg",
+                SaveToAlbum = true
+            });
+
+            if (photo != null)
+            {
+                Foto.Source = ImageSource.FromStream(() => 
+                {
+                    return photo.GetStream();
+                });
+            }
+        }
+    }
+}
